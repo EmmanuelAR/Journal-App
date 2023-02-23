be984cf92c2c67c609a2bfa875d32f8648e414fc
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-02-23 09:47:23 -0600

    Adding login code page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,39 @@
+import { Grid, TextField, Typography } from "@mui/material";
+import { padding } from "@mui/system";
 import React from "react";
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Login
+        </Typography>
+        <form>
+          <Grid container>
+            <Grid item>
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder="correo@google.com"
+                fullWidth
+              ></TextField>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
