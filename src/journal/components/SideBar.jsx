092217a482f4ddcb5a08ee8170f85232d3e559ae
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-02-28 09:55:28 -0600

    Adding the sideBar

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,59 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap componet="div">
+            Emmanuel Aguero
+          </Typography>
+        </Toolbar>
+        <Divider />
+
+        <List>
+          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText
+                    secondary={
+                      "Voluptate fugiat ex ad exercitation eu reprehenderit ex enim."
+                    }
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
