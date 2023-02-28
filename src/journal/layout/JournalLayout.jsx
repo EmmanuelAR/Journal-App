c4aa32defabdf6088f89c199cc22f7c38c0a9f16
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-02-28 09:56:07 -0600

    Adding the sidebar and navBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,16 +1,20 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import { NavBar, SideBar } from "../components";
 
-const drawerWith = 240;
+const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/* Navbar */}
+      {/* Navbar drawerWidth*/}
+      <NavBar drawerWidth={drawerWidth} />
 
-      {/* SideBar */}
+      {/* SideBar drawerWidth*/}
+      <SideBar drawerWidth={drawerWidth} />
+
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/* toolBar */}
+        <Toolbar />
         {children}
       </Box>
     </Box>
