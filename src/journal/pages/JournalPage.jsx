a5e6265266be556067db07fb0e029d716d9f0619
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-03-01 10:47:35 -0600

    Using noteView and nothingView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,16 @@
 import { MailOutline } from "@mui/icons-material";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import React from "react";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <Typography>
-          Consequat mollit irure laboris eu tempor aliquip ea qui ullamco
-          proident proident nostrud esse sint.
-        </Typography>
+        <NothingSelectedView />
+        {/* <NoteView /> */}
+        <IconButton size="large" sx={{ color: "white" }} />
       </JournalLayout>
     </>
   );
