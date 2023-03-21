52bc981ac08d922c9355c2bf5ee4d7c5d22d6a01
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-03-21 12:44:53 -0600

    Use memo for the status of the app, and buttons disabled.

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Google } from "@mui/icons-material";
@@ -6,8 +6,14 @@ import { useForm } from "../../hooks/useForm";
 
 import { AuthLayout } from "../layout/AuthLayout";
 import { checkingAuthentication, startGoogleSignIn } from "../../store/auth";
+import { useMemo } from "react";
 
 export const LoginPage = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  //Si el estatus cambia lo actualizo.
+  const isAuthenticating = useMemo(() => status === "checking", [status]);
+
   const dispatch = useDispatch();
   const { email, password, onInputChange } = useForm({
     email: "emmanuel@gmail.com",
@@ -53,12 +59,22 @@ export const LoginPage = () => {
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button type="submit" variant="contained" fullWidth>
+              <Button
+                disabled={isAuthenticating}
+                type="submit"
+                variant="contained"
+                fullWidth
+              >
                 Login
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth onClick={onGoogleSignIn}>
+              <Button
+                disabled={isAuthenticating}
+                variant="contained"
+                fullWidth
+                onClick={onGoogleSignIn}
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
