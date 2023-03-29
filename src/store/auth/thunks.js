d8dd161678135ac0e539627afafe6991dbbb493c
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-03-28 21:17:00 -0600

    Register email and password  calling google

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,7 @@
-import { signInWithGoogle } from "../../firebase/providers";
+import {
+  registerUserWithEmailPassword,
+  signInWithGoogle,
+} from "../../firebase/providers";
 import { checkingCredencials, login, logout } from "./authSlice";
 
 export const checkingAuthentication = (email, password) => {
@@ -13,5 +16,23 @@ export const startGoogleSignIn = () => {
     const result = await signInWithGoogle();
     if (!result.ok) return dispatch(logout(result));
     dispatch(login(result));
+  };
+};
+
+export const startCreatingUserWithEmailPassword = ({
+  email,
+  password,
+  displayName,
+}) => {
+  return async (dispatch) => {
+    dispatch(checkingCredencials());
+    const { ok, uid, photoURL, errorMessage } =
+      await registerUserWithEmailPassword({
+        email,
+        password,
+        displayName,
+      });
+    if (!ok) return dispatch(logout({ errorMessage }));
+    dispatch(login({ uid, displayName, email, photoURL }));
   };
 };
