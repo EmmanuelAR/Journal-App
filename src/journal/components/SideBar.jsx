92d69146da108f424759729e05d7029af2fd2273
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-04-24 10:23:40 -0600

    Adding active note and side bar item

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,21 +1,11 @@
-import { TurnedInNot } from "@mui/icons-material";
-import {
-  Divider,
-  Drawer,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { Divider, Drawer, List, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({ drawerWidth = 240 }) => {
   const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((notes) => notes.journal);
 
   return (
     <Box
@@ -38,22 +28,8 @@ export const SideBar = ({ drawerWidth = 240 }) => {
         <Divider />
 
         <List>
-          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={
-                      "Voluptate fugiat ex ad exercitation eu reprehenderit ex enim."
-                    }
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+            <SideBarItem key={note.id} {...note} />
           ))}
         </List>
       </Drawer>
