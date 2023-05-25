685b1d293320c3f82e9596310a212a1c9a3a971b
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-05-25 11:19:35 -0600

    Config env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getEnvironments } from "../helpers";
+
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
+
+const firebaseConfig = {
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
+};
 
 // //DEV-PROD
 // const firebaseConfig = {
@@ -13,14 +32,14 @@ import { getFirestore } from "firebase/firestore/lite";
 // };
 
 //TESTING
-const firebaseConfig = {
-  apiKey: "AIzaSyA-p_XcTUdYdw38x2zTsVOitBZfzc2bicQ",
-  authDomain: "react-course-proddb.firebaseapp.com",
-  projectId: "react-course-proddb",
-  storageBucket: "react-course-proddb.appspot.com",
-  messagingSenderId: "143814826238",
-  appId: "1:143814826238:web:2d6e54a4bf65bfe713adaf",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA-p_XcTUdYdw38x2zTsVOitBZfzc2bicQ",
+//   authDomain: "react-course-proddb.firebaseapp.com",
+//   projectId: "react-course-proddb",
+//   storageBucket: "react-course-proddb.appspot.com",
+//   messagingSenderId: "143814826238",
+//   appId: "1:143814826238:web:2d6e54a4bf65bfe713adaf",
+// };
 
 export const FireBaseApp = initializeApp(firebaseConfig);
 export const FireBaseAuth = getAuth(FireBaseApp);
